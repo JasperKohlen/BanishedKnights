2927459fcc4d17292746202aaac759ddf4020bd4
jkohlen <386329@student.fontys.nl>
2021-01-26 14:44:08 +0100

    Full camera functionality
    Including zoom, rotation, drag

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,24 +4,80 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform cameraTransform;
+
     //Player can choose speed of camera by pressing shift
     public float slowSpeed;
     public float normalSpeed;
 
     private float movementSpeed;
     public float movementTime;
+    public float rotationAmount;
+    public Vector3 zoomAmount;
 
-    public Vector3 newPosition;
+    private Quaternion newRotation;
+    private Vector3 newPosition;
+    private Vector3 newZoom;
+
+    private Vector3 dragStartPosition;
+    private Vector3 dragCurrentPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         newPosition = transform.position;
+        newRotation = transform.rotation;
+        //Keep camera relevant to camera rig
+        newZoom = cameraTransform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        HandleMouseInput();
         HandleMovementInput();
+    }
+
+    void HandleMouseInput()
+    {
+        //Handle scroll zoom
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * -zoomAmount;
+        }
+
+        //Handle camera drag with mouse
+        //Check if button is pressed
+        if (Input.GetMouseButtonDown(1))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+        //Check if button is held down
+        if (Input.GetMouseButton(1))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                //Determine new position by taking the difference between start and current drag position
+                newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
     }
 
     void HandleMovementInput()
@@ -52,7 +108,29 @@ public class CameraController : MonoBehaviour
             newPosition += (transform.right * -movementSpeed);
         }
 
-        //Make the camera smooth
+        //Handle rotation
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        ////Handle keyboard zoom
+        //if (Input.GetKey(KeyCode.R))
+        //{
+        //    newZoom += zoomAmount;
+        //}
+        //if (Input.GetKey(KeyCode.F))
+        //{
+        //    newZoom -= zoomAmount;
+        //}
+
+        //Update camera + Make camera smooth
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
     }
 }
