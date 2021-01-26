a08f72f1d780483fb1b93e58f04cf3d12efc15c1
jkohlen <386329@student.fontys.nl>
2021-01-26 14:07:49 +0100

    Added camera movements

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Player can choose speed of camera by pressing shift
+    public float slowSpeed;
+    public float normalSpeed;
+
+    private float movementSpeed;
+    public float movementTime;
+
+    public Vector3 newPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+    }
+
+    void HandleMovementInput()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = slowSpeed;
+        }
+        else
+        {
+            movementSpeed = normalSpeed;
+        }
+        //WASD and Arrow key support
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -movementSpeed);
+        }
+
+        //Make the camera smooth
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+    }
+}
