460b5975c6a9160bda45b9be2523b007dcabea11
jkohlen <386329@student.fontys.nl>
2021-01-27 14:19:20 +0100

    Can place house and storage
    No grid implemented yet

---
+++ Assets/Scripts/Build_House.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build_House : MonoBehaviour
+{
+    public GameObject houseBlueprint;
+    public GameObject storageBlueprint;
+
+    public void SpawnHouseBlueprint()
+    {
+        Instantiate(houseBlueprint);
+    }
+
+    public void SpawnStorageBlueprint()
+    {
+        Instantiate(storageBlueprint);
+    }
+}
---
+++ Assets/Scripts/blueprintPlacement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blueprintPlacement : MonoBehaviour
+{
+    private RaycastHit hit;
+    Vector3 movePoint;
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePrefab();
+        RotatePrefab();
+        PlacePrefab();
+    }
+    private void RotatePrefab()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.Rotate(0, 45, 0);
+        }
+    }
+
+    private void MovePrefab()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Cast a ray, when it hits a collider -> return hitinfo
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = hit.point;
+            //Adjust building's rotation based on terrain
+            //transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        }
+    }
+
+    private void PlacePrefab()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+}
