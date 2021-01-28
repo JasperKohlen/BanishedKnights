bdc1e4bd46fedf2173753391bebecd8dd2c21418
jkohlen <386329@student.fontys.nl>
2021-01-28 16:13:24 +0100

    Spawning/moving clouds and selection tool
    Selection box doesnt work and clouds cant randomly rotate yet

---
+++ Assets/Scripts/SelectedDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedDictionary : MonoBehaviour
+{
+    public Dictionary<int, GameObject> selectedTable = new Dictionary<int, GameObject>();
+
+    public void AddSelected(GameObject go)
+    {
+        int id = go.GetInstanceID();
+
+        if (!selectedTable.ContainsKey(id))
+        {
+            selectedTable.Add(id, go);
+            go.AddComponent<SelectionComponent>();
+        }
+    }
+
+    public void Deselect(int id)
+    {
+        Destroy(selectedTable[id].GetComponent<SelectionComponent>());
+        selectedTable.Remove(id);
+    }
+
+    public void DeselectAll()
+    {
+        foreach (KeyValuePair<int, GameObject> pair in selectedTable)
+        {
+            //Check if object has not been destroyed yet
+            if (pair.Value != null)
+            {
+                Destroy(selectedTable[pair.Key].GetComponent<SelectionComponent>());
+            }
+        }
+        //Table needs to be cleared on top of destroy selectioncomponents
+        selectedTable.Clear();
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIManager : MonoBehaviour
+{
+    RemovableSelection removableSelection;
+    SelectedDictionary selectedTable;
+
+    public GameObject removeDestroyPanel;
+    private void Start()
+    {
+        removableSelection = EventSystem.current.gameObject.GetComponent<RemovableSelection>();
+        selectedTable = EventSystem.current.gameObject.GetComponent<SelectedDictionary>();
+
+    }
+    //Open the menu with all remove and destroy buttons
+    public void HandleRemoveDestroyPanel()
+    {
+        if (removeDestroyPanel.activeSelf)
+        {
+            Debug.Log("Turning off menu");
+            removeDestroyPanel.SetActive(false);
+        }
+        else if (!removeDestroyPanel.activeSelf)
+        {
+            Debug.Log("Turning on menu");
+            removeDestroyPanel.SetActive(true);
+        }
+    }
+
+    //Enable/Disable resource removal selection
+    public void HandleRemovableSelection()
+    {
+        if (removableSelection.enabled == true)
+        {
+            removableSelection.enabled = false;
+            selectedTable.DeselectAll();
+        }
+        else 
+        {
+            removableSelection.enabled = true;
+        }
+    }
+}
